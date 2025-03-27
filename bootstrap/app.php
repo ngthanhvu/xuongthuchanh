a4b4b37e0eb588fc06d81859f3380ca081d4c4f9
hoaily2005 <hoailxpk03594@gmail.com>
2025-03-27 10:33:23 +0700

    fix looix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,9 +17,6 @@ return Application::configure(basePath: dirname(__DIR__))
             'check.role' => CheckAdmin::class,
         ]));
 
-        $middleware->alias(([
-            'check.enrollment' => CheckEnrollment::class,
-        ]));
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
